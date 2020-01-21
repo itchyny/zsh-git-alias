02001e0e304778e4f203f31d64f527502ca88c4c
itchyny <itchyny@hatena.ne.jp>
2020-01-21 14:35:57 +0900

    join lines of gcd (zsh-syntax-highlighting stops working for this)

--- zsh-git-alias.zsh
+++ zsh-git-alias.zsh
@@ -4,9 +4,7 @@ alias gb='git branch'
 alias gc='git checkout'
 alias gcb='git checkout -b'
 alias gcm='git checkout master'
-alias gcd='git checkout develop 2>/dev/null ||
-  git checkout staging 2>/dev/null ||
-  git checkout master'
+alias gcd='git checkout develop 2>/dev/null || git checkout staging 2>/dev/null || git checkout master'
 
 alias ga='git add'
 alias gs='git status'
